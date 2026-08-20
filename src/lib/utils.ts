579780c46a1821c47539191f8fd8a64cddc747fb
Vatistas Dimitris <vatistasdimitris.01@gmail.com>
2026-08-20 11:57:17 +0300

    Add utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date | string) {
+  const d = typeof date === "string" ? new Date(date) : date;
+  return d.toLocaleDateString(undefined, {
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
